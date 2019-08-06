ef657f6cd842eeb44e1761e7b854b2ef6cfc3358
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-08-06 19:31:31 +0700

    Support MultiEpisode xattr

--- Scanners/Series/FileBot Xattr Series Scanner.py
+++ Scanners/Series/FileBot Xattr Series Scanner.py
@@ -11,24 +11,30 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
     if attr is None:
       continue
 
-    guid = series_guid(attr)
-    if guid is None:
-      continue
+    episodes = list_episodes(attr)
+    for i, attr in enumerate(episodes):
+      guid = series_guid(attr)
+      if guid is None:
+        continue
 
-    episode = Media.Episode(
-      str(series_name(attr)),
-      episode_season_number(attr),
-      episode_number(attr),
-      str(episode_title(attr)),
-      series_year(attr)
-    )
+      episode = Media.Episode(
+        str(series_name(attr)),
+        episode_season_number(attr),
+        episode_number(attr),
+        str(episode_title(attr)),
+        series_year(attr)
+      )
 
-    date = episode_date(attr)
-    if date:
-      episode.released_at = date.strftime('%Y-%m-%d')
+      date = episode_date(attr)
+      if date:
+        episode.released_at = date.strftime('%Y-%m-%d')
 
-    episode.parts.append(file)
-    mediaList.append(episode)
+      if (len(episodes) > 1):
+        episode.display_offset = (i * 100) / len(episodes)
+
+      print(episode)
+      episode.parts.append(file)
+      mediaList.append(episode)
 
 
 
@@ -43,6 +49,8 @@ if __name__ == "__main__":
         file = os.path.join(root, name)
         attr = xattr_metadata(file)
         if attr:
-          guid = series_guid(attr)
-          if guid:
-            print "%s\t%s\t%s" % (guid, attr, file)
+          episodes = list_episodes(attr)
+          for episode in episodes:
+            guid = series_guid(episode)
+            if guid:
+              print "%s\t%s\t%s" % (guid, episode, file)

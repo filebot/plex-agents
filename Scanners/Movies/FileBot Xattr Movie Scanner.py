0fa83a511271b9400598cfe3ef4e6026b19f3278
Reinhard Pointner <reinhard.pointner@gmail.com>
2019-08-07 16:07:52 +0700

    Support multi-part movies

--- Scanners/Movies/FileBot Xattr Movie Scanner.py
+++ Scanners/Movies/FileBot Xattr Movie Scanner.py
@@ -6,6 +6,10 @@ from filebot import *
 def Scan(path, files, mediaList, subdirs, language=None, root=None):
   VideoFiles.Scan(path, files, mediaList, subdirs, root)
 
+  # group movie parts as they come in in sequence
+  prev_movie = None
+  prev_part_index = None
+
   for file in files:
     attr = xattr_metadata(file)
     if attr is None:
@@ -14,6 +18,14 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
     guid = movie_guid(attr)
     if guid is None:
       continue
+
+    part_index = movie_part_index(attr)
+    if prev_part_index and part_index and prev_part_index + 1 == part_index and prev_movie.guid == guid:
+      prev_part_index = part_index
+      prev_movie.parts.append(file)
+      print("[XATTR] %s | Part %s | %s" % (prev_movie, part_index, attr))
+      continue
+
 
     movie = Media.Movie(
       str(movie_name(attr)),  # use str() since Plex doesn't like unicode strings
@@ -28,4 +40,11 @@ def Scan(path, files, mediaList, subdirs, language=None, root=None):
     movie.parts.append(file)
     mediaList.append(movie)
 
-    print("[XATTR] %s | %s | %s" % (movie, movie.guid, movie.source))
+    if part_index == 1:
+      prev_movie = movie
+      prev_part_index = 1
+    else:
+      prev_movie = None
+      prev_part_index = None
+
+    print("[XATTR] %s | %s | %s | %s" % (movie, movie.guid, movie.source, attr))
